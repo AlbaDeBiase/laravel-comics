5cbb9516b33ed8a5de0a1ab80e78a7e4822c4333
Alba De biase <debiasealba@gmail.com>
2021-01-09 21:48:59 +0100

    finito footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,102 @@
-<footer></footer>
+<footer>
+    <div class="container">
+        <div class="footer">
+            <div class="menu-footer">
+                <div class="dc-comics">
+                    <h1 class="uppercase">dc comics</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                    <h1 class="uppercase">shop</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="dc">
+                    <h1 class="uppercase">dc</h1>
+                        <ul>
+                            <li>
+                                <a href="#">Therms of use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshop</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                </div>
+                <div class="sites">
+                    <h1 class="uppercase">Sites</h1>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li><li>
+                                <a href="#">MAD Magazine</a>
+                            </li><li>
+                                <a href="#">DC Kids</a>
+                            </li><li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+            <div class="copy-right">
+                <p>
+                    All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.
+                </p>
+                <p><a href="#">Cookies Settings</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
