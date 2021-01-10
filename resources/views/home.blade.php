e24d77b2d4f90e3afc2eb2ad3331a63201ba5cd1
Alba De biase <debiasealba@gmail.com>
2021-01-10 03:08:22 +0100

    finito banner  buy services

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 @include('partials.jumbo')
 
+{{-- current serie in position absolute --}}
+    <div class="series">
+        <div class="current-series">
+                <h2 class="uppercase">current series</h2>
+        </div>
+    </div>
+
+    {{-- inizio contenitore cards --}}
     <div class="cards-container">
         <div class="container">
             <div class="fumetti">
@@ -20,6 +28,20 @@
         </div>
         <div class="button-cards">
             <button type="button" name="button"> <a class="uppercase"href="#">load more</a></button>
+        </div>
+    </div>
+    <div class="container-buy-services">
+        <div class="buy-services">
+            <img src="{{'/img/buy-comics-digital-comics.png'}}" alt="">
+                <span class="uppercase">digital comics</span>
+            <img src="{{'/img/buy-comics-merchandise.png'}}" alt="">
+                <span class="uppercase">dc merchandise</span>
+            <img src="{{'/img/buy-comics-subscriptions.png'}}" alt="">
+                <span class="uppercase">subscription</span>
+            <img src="{{'/img/buy-comics-shop-locator.png'}}" alt="">
+                <span class="uppercase">comic shop locator</span>
+            <img src="{{'/img/buy-dc-power-visa.svg'}}" alt="">
+                <span class="uppercase">dc power visa</span>
         </div>
     </div>
 
