7dbf0a37854ccf3c844a6d8422266d6cd545e97f
Alba De biase <debiasealba@gmail.com>
2021-01-10 21:43:55 +0100

    finita section con info price e availability

--- resources/views/dettaglio-fumetto.blade.php
+++ resources/views/dettaglio-fumetto.blade.php
@@ -17,6 +17,29 @@
         </div>
     </div>
 </div>
+<div class="container-dettaglio-fumetto">
+    <div class="info">
+        <div class="comic-description">
+            <h1 class="uppercase">{{$fumettoId['title']}}</h1>
+            <div class="title-info">
+                <div class="price">
+                    <span>U.S Price: {{$fumettoId['price']}}</span>
+                </div>
+                <div class="check-availability">
+                    <span class="uppercase">available</span>
+                    <span>Check Availability &#9662;</span>
+                </div>
+            </div>
+            <div class="description-info">
+                <p>{{$fumettoId['description']}}</p>
+            </div>
+        </div>
+        <div class="pubblicity">
+            <h2 class="uppercase">advertisement</h2>
+            <img src="{{'/img/adv.jpg'}}" alt="{{$fumettoId['series']}}">
+        </div>
+    </div>
+</div>
 
 
 @endsection
