ec879b97d05fa60b7fed5ca156519ea66efca303
Alba De biase <debiasealba@gmail.com>
2021-01-10 23:40:09 +0100

    finita sezione tabs

--- resources/views/dettaglio-fumetto.blade.php
+++ resources/views/dettaglio-fumetto.blade.php
@@ -40,7 +40,54 @@
         </div>
     </div>
 </div>
-
+{{-- fine section price e availabily  --}}
+{{-- inizio tabella artist --}}
+<div class="wrap-tabs">
+    <div class="container-dettaglio-fumetto">
+        <div class="tabs-artist">
+            <div class="talent">
+                <h2>Talent</h2>
+                <div class="art-by">
+                    <span class="title-tab-artist">Art by:</span>
+                    @foreach ($fumettoId['artists'] as $single_artist)
+                    <span class="block">
+                        <a href="#">{{$single_artist}}</a>{{ !$loop->last ? ',' : '' }}
+                    </span>
+                    @endforeach
+                </div>
+                <div class="written-by">
+                    <span class="title-tab-artist">Written by:</span>
+                    @foreach ($fumettoId['writers'] as $single_writer)
+                    <span class="block">
+                        <a href="#">{{$single_writer}}</a>{{ !$loop->last ? ',' : '' }}
+                    </span>
+                    @endforeach
+                </div>
+            </div>
+            <div class="specs">
+                <h2>Specs</h2>
+                <div class="series-name">
+                    <span class="title-tab-artist">Series</span>
+                    <span class="block">
+                        <a href="#">{{$fumettoId['series']}}</a>
+                    </span>
+                </div>
+                <div class="us-price">
+                    <span class="title-tab-artist">U.S. Price</span>
+                    <span class="block">
+                        <a href="#">{{$fumettoId['price']}}</a>
+                    </span>
+                </div>
+                <div class="on-sale-date">
+                    <span class="title-tab-artist">On Sale Date</span>
+                    <span class="block">
+                        <a href="#">{{$fumettoId['sale_date']}}</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
